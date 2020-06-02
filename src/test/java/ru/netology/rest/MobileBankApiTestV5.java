5e42792e7397a4bcaf94d15d37c450cf2f05bef1
ZhukovaAnna <zhukova.anna.home@gmail.com>
2020-06-02 14:19:13 +0300

    Add test

---
+++ src/test/java/ru/netology/rest/MobileBankApiTestV5.java
@@ -0,0 +1,26 @@
+package ru.netology.rest;
+
+import io.restassured.http.ContentType;
+import org.junit.Test;
+
+import static com.sun.org.apache.xerces.internal.util.FeatureState.is;
+import static io.restassured.RestAssured.given;
+
+public class MobileBankApiTestV5 {
+    @Test
+    void shouldReturnDemoAccounts() {
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                // Выполняемые действия
+                .when()
+                .get("/demo/accounts")
+                // Проверки
+                .then()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("every{ it.balance >= 0 }",is(true))
+        ;
+    }
+}
