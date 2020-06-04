5c0d51a2fda87af29a5bcd6a70882c1d503481ad
ZhukovaAnna <zhukova.anna.home@gmail.com>
2020-06-04 15:40:00 +0300

    Add json

--- src/test/java/ru/netology/rest/MobileBankApiTestV4.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV4.java
@@ -18,6 +18,7 @@ public class MobileBankApiTestV4 {
                 // Проверки
                 .then()
                 .statusCode(200)
+                // static import для JsonSchemaValidator.matchesJsonSchemaInClasspath
                 .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
         ;
     }
